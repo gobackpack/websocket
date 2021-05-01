b4de9aa9e09adbede4257fc2fc142672b757bdae
Semir Mahovkic <semirm.dev@gmail.com>
2021-05-01 08:41:20 +0200

    gobackpack

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-playground/validator/v10 v10.5.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
--- hub.go
+++ hub.go
@@ -2,6 +2,7 @@ package websocket
 
 import (
 	"encoding/json"
+	"github.com/google/uuid"
 	websocketLib "github.com/gorilla/websocket"
 	"github.com/sirupsen/logrus"
 	"net/http"
@@ -23,27 +24,28 @@ var upgrader = websocketLib.Upgrader{
 type Hub struct {
 	Connect    chan *Client
 	Disconnect chan *Client
-	Clients    map[string]*websocketLib.Conn
+	Clients    map[string]map[string]*websocketLib.Conn
 	Message    chan *Frame
 	ReadLock   sync.Mutex
 	SendLock   sync.Mutex
 }
 
 type Client struct {
+	GroupId      string
 	ConnectionId string
 	Connection   *websocketLib.Conn
 }
 
 type Frame struct {
-	ConnectionId string
-	Content      string
+	GroupId string
+	Content string
 }
 
 func NewHub() *Hub {
 	return &Hub{
 		Connect:    make(chan *Client),
 		Disconnect: make(chan *Client),
-		Clients:    make(map[string]*websocketLib.Conn, 0),
+		Clients:    make(map[string]map[string]*websocketLib.Conn, 0),
 		Message:    make(chan *Frame, 0),
 	}
 }
@@ -59,29 +61,39 @@ func (hub *Hub) ListenConnections(done chan bool) chan bool {
 		for {
 			select {
 			case client := <-hub.Connect:
-				hub.Clients[client.ConnectionId] = client.Connection
-				logrus.Info("client connected: ", client.ConnectionId)
+				if hub.Clients[client.GroupId] == nil {
+					hub.Clients[client.GroupId] = make(map[string]*websocketLib.Conn, 0)
+				}
+				clientId := uuid.New().String()
+				hub.Clients[client.GroupId][clientId] = client.Connection
+				logrus.Infof("client [%v] connected to group [%v]", clientId, client.GroupId)
 				break
 			case client := <-hub.Disconnect:
-				delete(hub.Clients, client.ConnectionId)
-				logrus.Warn("client disconnected: ", client.ConnectionId)
+				if hub.Clients[client.GroupId] != nil {
+					delete(hub.Clients[client.GroupId], client.ConnectionId)
+					logrus.Warnf("client [%v] disconnected from group [%v]: ", client.ConnectionId, client.GroupId)
+				}
 				break
 			case frame := <-hub.Message:
-				b, err := json.Marshal(frame)
-				if err != nil {
-					logrus.Error("failed to marshal hub message: ", err)
-					break
-				}
+				if hub.Clients[frame.GroupId] != nil {
+					b, err := json.Marshal(frame)
+					if err != nil {
+						logrus.Error("failed to marshal hub message: ", err)
+						break
+					}
 
-				conn := hub.Clients[frame.ConnectionId]
-				if conn == nil {
-					logrus.Error("client connection is null")
-					break
-				}
+					group := hub.Clients[frame.GroupId]
+					if group == nil {
+						logrus.Error("client group is null")
+						break
+					}
 
-				if err := hub.send(conn, TextMessage, b); err != nil {
-					logrus.Error("failed to send message: ", err)
-					break
+					for _, conn := range group {
+						if err := hub.send(conn, TextMessage, b); err != nil {
+							logrus.Error("failed to send message: ", err)
+							break
+						}
+					}
 				}
 				break
 			case <-done:
@@ -93,15 +105,15 @@ func (hub *Hub) ListenConnections(done chan bool) chan bool {
 	return cancelled
 }
 
-func (hub *Hub) EstablishConnection(w http.ResponseWriter, r *http.Request, connectionId string) error {
+func (hub *Hub) EstablishConnection(w http.ResponseWriter, r *http.Request, groupId string) error {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return err
 	}
 
 	client := &Client{
-		ConnectionId: connectionId,
-		Connection:   conn,
+		GroupId:    groupId,
+		Connection: conn,
 	}
 
 	hub.Connect <- client
@@ -113,15 +125,15 @@ func (hub *Hub) EstablishConnection(w http.ResponseWriter, r *http.Request, conn
 
 func (hub *Hub) ReadMessages(client *Client) {
 	defer func() {
+		hub.Disconnect <- client
+
 		logrus.Warn("websocket connection stopped reading messages")
 
 		if err := client.Connection.Close(); err != nil {
 			logrus.Error("failed to close websocket connection: ", err)
-		} else {
-			logrus.Warn("websocket connection closed")
 		}
 
-		hub.Disconnect <- client
+		logrus.Warn("websocket connection closed")
 	}()
 
 	for {
@@ -136,10 +148,10 @@ func (hub *Hub) ReadMessages(client *Client) {
 	}
 }
 
-func (hub *Hub) SendMessage(connectionId string, msg []byte) error {
+func (hub *Hub) SendMessage(groupId string, msg []byte) error {
 	frame := &Frame{
-		ConnectionId: connectionId,
-		Content:      string(msg),
+		GroupId: groupId,
+		Content: string(msg),
 	}
 
 	hub.Message <- frame
