88b407436e0a6f884fca461374e2eb7379552cd1
Semir Mahovkic <semirm.dev@gmail.com>
2021-05-16 22:56:43 +0200

    websocket

--- hub_test.go
+++ hub_test.go
@@ -9,6 +9,32 @@ import (
 	"testing"
 )
 
+
+func BenchmarkHub_SendToGroup(b *testing.B) {
+	hub := websocket.NewHub()
+
+	done := make(chan bool)
+	cancelled := hub.ListenConnections(done)
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := hub.EstablishConnection(w, r, "1", "1")
+			if err != nil {
+				logrus.Errorf("failed to establish connection with groupId -> %s", "1")
+				return
+			}
+		}))
+
+	defer ts.Close()
+
+	for n := 0; n < b.N; n++ {
+		go hub.SendToGroup("1", []byte(fmt.Sprint(n)))
+	}
+
+	close(done)
+	<-cancelled
+}
+
 func BenchmarkHub_SendToAllGroups(b *testing.B) {
 	hub := websocket.NewHub()
 
@@ -34,4 +60,75 @@ func BenchmarkHub_SendToAllGroups(b *testing.B) {
 	<-cancelled
 }
 
+func BenchmarkHub_SendToConnectionId(b *testing.B) {
+	hub := websocket.NewHub()
 
+	done := make(chan bool)
+	cancelled := hub.ListenConnections(done)
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := hub.EstablishConnection(w, r, "1", "1")
+			if err != nil {
+				logrus.Errorf("failed to establish connection with groupId -> %s", "1")
+				return
+			}
+		}))
+
+	defer ts.Close()
+
+	for n := 0; n < b.N; n++ {
+		go hub.SendToConnectionId("1", "1", []byte(fmt.Sprint(n)))
+	}
+
+	close(done)
+	<-cancelled
+}
+
+func BenchmarkHub_SendToOthersInGroup(b *testing.B) {
+	hub := websocket.NewHub()
+
+	done := make(chan bool)
+	cancelled := hub.ListenConnections(done)
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := hub.EstablishConnection(w, r, "1", "1")
+			if err != nil {
+				logrus.Errorf("failed to establish connection with groupId -> %s", "1")
+				return
+			}
+		}))
+
+	defer ts.Close()
+
+	for n := 0; n < b.N; n++ {
+		go hub.SendToOthersInGroup("1", "1", []byte(fmt.Sprint(n)))
+	}
+
+	close(done)
+	<-cancelled
+}
+
+func BenchmarkHub_EstablishConnection(b *testing.B) {
+	hub := websocket.NewHub()
+
+	done := make(chan bool)
+	cancelled := hub.ListenConnections(done)
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			for n := 0; n < b.N; n++ {
+				_, err := hub.EstablishConnection(w, r, "1", "1")
+				if err != nil {
+					logrus.Errorf("failed to establish connection with groupId -> %s", "1")
+					return
+				}
+			}
+		}))
+
+	defer ts.Close()
+
+	close(done)
+	<-cancelled
+}
